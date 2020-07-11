d4f6f9c971939495bcfe891348f3d7a1275c9251
mo30coder <mo30coder@gmail.com>
2020-07-12 00:21:43 +0430

    Add Gorm and some structs

--- src/main.go
+++ src/main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 )
 
+type Game struct {
+	id uint
+	gorm.Model
+	Name  string
+	Genre []Genre
+}
+
+type Genre struct {
+	id    uint
+	Title string
+}
+
 func main() {
+
+	_, err := gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic(err.Error())
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", helloWorld)
