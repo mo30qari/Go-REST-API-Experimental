ca67b7ee75943a9b3a67e20d9e7466568c9ea707
mo30coder <mo30coder@gmail.com>
2020-07-12 00:04:03 +0430

    Initialize

--- src/main.go
+++ src/main.go
@@ -3,5 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm"
+	"log"
+	"net/http"
 )
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloWorld)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Hello World")
+
+}
