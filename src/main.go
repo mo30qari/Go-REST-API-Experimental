82a7374412dc2547ca983c431fce5fec40cdcd1e
mostafa qari <mo30qari@gmail.com>
2020-07-12 10:35:23 +0430

    Add read all the games

--- src/main.go
+++ src/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 )
@@ -24,21 +23,33 @@ type Genre struct {
 
 func main() {
 
-	_, err := gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
 	router := mux.NewRouter()
-	router.HandleFunc("/", helloWorld)
+	router.HandleFunc("/games", getAllGames)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
+func getAllGames(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("Hello World")
+	db, err := getDB()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game := Game{}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	result := db.Debug().Find(&game)
+
+	json.NewEncoder(w).Encode(result)
+
+}
+
+func getDB() (*gorm.DB, error) {
+
+	return gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
 
 }
