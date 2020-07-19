1fa645afa6a81cc1792f739b4690daff8ec975fb
mostafa qari <mo30qari@gmail.com>
2020-07-19 10:33:18 +0430

    Understand Relation in Gorm

--- src/main.go
+++ src/main.go
@@ -1,106 +1,56 @@
 package main
 
 import (
-	"fmt"
-
+	_ "encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
-)
-
-const (
-	connStr = "root:@/dds_db?charset=utf8&parseTime=True&loc=Local"
+	"log"
+	"net/http"
 )
 
 type (
-	User struct {
+	Game struct {
 		gorm.Model
-		Name     string
-		Username string `gorm:"not null"`
-		Password string `gorm:"not null"`
-		Messages []Message
+		Name    string
+		Genre   Genre
+		GenreID string
 	}
-
-	Message struct {
-		Body   string `gorm:"not null`
-		User   User
-		UserID uint
+	
+	Genre struct {
+		Title string
 		gorm.Model
 	}
 )
 
 func main() {
-
-	db, err := gorm.Open("mysql", connStr)
-	db.DropTableIfExists(&Message{}, &User{})
-
-	if err != nil {
-		fmt.Println(err.Error())
+	db, _ := getDB()
+	db.DropTableIfExists(&Game{}, &Genre{})
+	
+	db.AutoMigrate(&Game{})
+	db.AutoMigrate(&Genre{})
+	
+	game := &Game{
+		Name: "Drop Puzzle",
 	}
-	defer db.Close()
-
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Message{})
-
-	var user = &User{Name: "Peter Jones", Username: "pj@email.com", Password: "password"}
-
-	if err := db.Model(&User{}).Create(user).Error; err != nil {
+	
+	if err := db.Model(&Game{}).Create(game).Error; err != nil {
+		
 		panic(err.Error())
+		
 	}
-
-	message := &Message{Body: "Hi"}
-
-	db.Model(user).Find(user)
-	db.Model(user).Association("Messages").Append(message)
+	
+	genre := &Genre{Title: "Arcade"}
+	
+	db.Model(game).Find(game)
+	db.Model(game).Association("Genre").Append(genre)
+	
+	router := mux.NewRouter()
+	//router.HandleFunc("/games", getAllGames)
+	
+	log.Fatal(http.ListenAndServe(":8080", router))
+	
 }
-
-//import (
-//	_ "encoding/json"
-//	_ "github.com/go-sql-driver/mysql"
-//)
-
-//type Genre struct {
-//	Title string
-//	gorm.Model
-//}
-//
-//type Game struct {
-//	Name  string
-//	Genre Genre
-//	gorm.Model
-//}
-//
-//func main() {
-//	db, _ := getDB()
-//	db.DropTableIfExists(&Game{}, &Genre{})
-//
-//	db.AutoMigrate(&Game{})
-//	db.AutoMigrate(&Genre{})
-//
-//	game := &Game{
-//		Name: "Drop Puzzle",
-//	}
-//
-//	if err := db.Model(&Game{}).Create(game).Error; err != nil{
-//
-//		panic(err.Error())
-//
-//	}
-//
-//	genre := &Genre{
-//		Title: "Arcade",
-//	}
-//
-//	db.Model(&Game{}).Find(game)
-//	db.Model(game).Association("Genre").Append(genre)
-//
-//
-//
-//	router := mux.NewRouter()
-//	//router.HandleFunc("/games", getAllGames)
-//
-//	log.Fatal(http.ListenAndServe(":8080", router))
-//
-//}
 
 //func getAllGames(w http.ResponseWriter, r *http.Request) {
 //
@@ -114,7 +64,7 @@ func main() {
 //
 //	result, err := db.Model(&game).Where("id = ?", "1").Select("id, created_at, deleted_at").Rows()
 //
-//	if err != nil{
+//	if err != nil {
 //		panic(err.Error())
 //	}
 //
@@ -122,7 +72,7 @@ func main() {
 //
 //		err = result.Scan(&id, &Name, &Genre{})
 //
-//		if err != nil{
+//		if err != nil {
 //			panic(err.Error())
 //		}
 //
@@ -130,8 +80,8 @@ func main() {
 //
 //}
 
-//func getDB() (*gorm.DB, error) {
-//
-//	return gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
-//
-//}
+func getDB() (*gorm.DB, error) {
+	
+	return gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
+	
+}
